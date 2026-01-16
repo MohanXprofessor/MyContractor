b7be3f98db40b328df3cf3717f0e2152a44894ef
user.name:"yadavmohan8600@gmail.com <user.email "yadavmohan8600.com>
2026-01-16 20:57:06 +0530

    alive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
-})
-export class HomeComponent {
 
-}
+  /* The line `'./home.component.html'` in the code is specifying the relative path to the
+  HTML template file for the Angular component. This file contains the markup that
+  defines the structure of the component's view. When the component is rendered,
+  Angular will use this HTML template to generate the corresponding DOM elements. */
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
